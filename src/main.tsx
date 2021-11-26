77db51b740b022a3c100687062d07df3bc024175
Denis Smirov <Dennicraft.Ds@gmail.com>
2021-11-26 14:48:42 +0300

    add errorBoundary

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
+import React from "react";
+import ReactDom from "react-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import ErrorBoundary from "./components/error/error";
+
 import "./styles/main.css";
 import "./styles/main.scss";
 // watch: native intellisense and file-peek for aliases from jsconfig.json and with none-js files doesn't work: https://github.com/microsoft/TypeScript/issues/29334
-import ReactDom from "react-dom";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
 import Home from "./components/main/homePage";
@@ -11,22 +15,43 @@ import Products from "./components/main/productsPage";
 import SignIn from "./components/main/signInPage";
 import SignUp from "./components/main/signUpPage";
 
-const App: React.FC = function () {
-  return (
-    <>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/login" element={<SignIn />} />
-        <Route path="/register" element={<SignUp />} />
-        <Route path="*" element={<Home />} />
-      </Routes>
-      <Footer />
-    </>
-  );
-};
+class App extends React.Component {
+  render() {
+    return (
+      <ErrorBoundary>
+        <>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/login" element={<SignIn />} />
+            <Route path="/register" element={<SignUp />} />
+            <Route path="*" element={<Navigate to="/" />} />;
+          </Routes>
+          <Footer />
+        </>
+      </ErrorBoundary>
+    );
+  }
+}
+
+// const App: React.FC = function () {
+//   return (
+//     <>
+//       <Header />
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/about" element={<About />} />
+//         <Route path="/products" element={<Products />} />
+//         <Route path="/login" element={<SignIn />} />
+//         <Route path="/register" element={<SignUp />} />
+//         <Route path="*" element={<Home />} />
+//       </Routes>
+//       <Footer />
+//     </>
+//   );
+// };
 
 console.log(<App />);
 
