fa7f10eaed01d9b8e230caf596eee2f5f4ca13d6
Denis Smirov <Dennicraft.Ds@gmail.com>
2021-12-06 15:35:21 +0300

    add apiURL to constants

--- src/api/mockapiData.tsx
+++ src/api/mockapiData.tsx
@@ -1,5 +1,7 @@
+import MOCK_API_URL from "@/constants/URL";
+
 const getGamesData = function (path: string) {
-  return fetch(`https://61a36f44d5e8330017292010.mockapi.io/${path}`).then((response) => response.json());
+  return fetch(`${MOCK_API_URL}${path}`).then((response) => response.json());
 };
 
 export default getGamesData;
